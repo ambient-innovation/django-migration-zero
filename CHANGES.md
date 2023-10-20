19a14c3403224c146f3e42ebc7cb77cb5dcdd89b
Ronny Vedrilla <ronny.vedrilla@ambient-innovation.com>
2023-10-20 16:31:42 +0200

    v0.1.6

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,8 @@
 # Changelog
 
+* *0.1.6** (2023-10-20)
+  * Fixes and adjustments
+*
 * *0.1.5** (2023-10-20)
   * Coverage
   * Dependency updates
--- django_migration_zero/__init__.py
+++ django_migration_zero/__init__.py
@@ -3,4 +3,4 @@
 Holistic implementation of "migration zero" pattern for Django covering local changes and in-production database adjustments.
 """
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
